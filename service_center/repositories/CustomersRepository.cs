50777a3e7b0cfd360a0c7f091f3cc95d766caa8a
Alexander Sokolov <sashkacosmonaut@gmail.com>
2021-07-14 14:10:24 +0300

    Переименован лист клиентов в Pascal Case.

--- service_center/repositories/CustomersRepository.cs
+++ service_center/repositories/CustomersRepository.cs
@@ -8,25 +8,24 @@ namespace service_center.repositories
 {
     public class CustomersRepository : IBaseCustomersRepository
     {
-        private static List<Customer> customer_list = new List<Customer>();
-
+        private static readonly List<Customer> CustomerList = new List<Customer>();
 
         public CustomersRepository()
         {
-            customer_list.Add(new Customer(1, "Al", "eng", DateTime.Now, "koko", 80808));
-            customer_list.Add(new Customer(2, "Bl", "man", DateTime.Now, "kiko", 88808));
-            customer_list.Add(new Customer(3, "Cl", "eng", DateTime.Now, "keko", 80888));
-            customer_list.Add(new Customer(4, "Dl", "dir", DateTime.Now, "kvko", 80800));
-            customer_list.Add(new Customer(5, "El", "eng", DateTime.Now, "kqko", 80000));
+            CustomerList.Add(new Customer(1, "Al", "eng", DateTime.Now, "koko", 80808));
+            CustomerList.Add(new Customer(2, "Bl", "man", DateTime.Now, "kiko", 88808));
+            CustomerList.Add(new Customer(3, "Cl", "eng", DateTime.Now, "keko", 80888));
+            CustomerList.Add(new Customer(4, "Dl", "dir", DateTime.Now, "kvko", 80800));
+            CustomerList.Add(new Customer(5, "El", "eng", DateTime.Now, "kqko", 80000));
         }
         
         public void AddCustomer(Customer new_customer)
         {
-            int id = customer_list.Count + 1;
-            customer_list.Add(new Customer(id, new_customer.name, new_customer.position_cus, DateTime.Now, new_customer.mail, new_customer.phone));
+            int id = CustomerList.Count + 1;
+            CustomerList.Add(new Customer(id, new_customer.name, new_customer.position_cus, DateTime.Now, new_customer.mail, new_customer.phone));
             //customer_list.Insert(id - 1, new customer(id, customer.name, customer.position_cus, DateTime.Now, customer.mail, customer.phone));
 
-            foreach (Customer customer in customer_list)
+            foreach (Customer customer in CustomerList)
             {
                 Console.WriteLine(customer);
             }
@@ -34,8 +33,8 @@ namespace service_center.repositories
 
         public void Delete(Customer ch_customer)
         {
-            customer_list.Remove(ch_customer);
-            foreach (Customer customer in customer_list)
+            CustomerList.Remove(ch_customer);
+            foreach (Customer customer in CustomerList)
             {
                 Console.WriteLine(customer);
             }
@@ -43,7 +42,7 @@ namespace service_center.repositories
 
         public Customer GetById(int id)
         {
-            Customer ch_customer = customer_list.Find(item => item.id_cus == id);
+            Customer ch_customer = CustomerList.Find(item => item.id_cus == id);
 
             return ch_customer;
         }
@@ -54,12 +53,12 @@ namespace service_center.repositories
             {
                 Console.WriteLine(ch_customer);
             }*/
-            return customer_list.ToArray();
+            return CustomerList.ToArray();
         }
 
         public void Update(Customer ch_customer)
         {
-            customer_list.
+            CustomerList.
                 FindAll(item => item.id_cus == ch_customer.id_cus).
                 ForEach(x => { x.name = ch_customer.name; x.position_cus = ch_customer.position_cus; x.mail = ch_customer.mail; x.phone = ch_customer.phone; } );
         }
