338817c8b114e74fd8b181f56a9a8de3ccab351b
youngformoza <vlassova.yulya@gmail.com>
2021-07-14 23:03:30 +0300

    Добавлен репозиторий для работы с БД услуг

---
+++ DB_service_Infrastructure/MySQLRepositories/MySQLServicesRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DB_Connections.Entities;
+using DB_Connections.Interfaces;
+using MySql.Data.MySqlClient;
+
+namespace DB_service_Infrastructure.MySQLRepositories
+{
+    class MySQLServicesRepository
+    {
+        protected string ConnectionString { get; set; }
+
+
+        public MySQLServicesRepository()
+        {
+            ConnectionString = "server=localhost;user=root;database=service_center;port=3306;password=2907Yu1909Ya";
+        }
+
+
+        public services GetByName(string name)
+        {
+            services ChService = null;
+
+            try
+            {
+                using var connection = new MySqlConnection(ConnectionString);
+
+                connection.Open();
+
+                using var command = new MySqlCommand("SELECT id_ser, name FROM services WHERE name = @name", connection);
+
+                command.Parameters.AddWithValue("name", name);
+
+                using var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ChService = new services(reader.GetInt32(0), reader.GetString(1));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return ChService;
+        }
+    }
+}
