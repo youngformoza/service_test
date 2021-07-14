b717e0bdcf0384080d241447fc6e9d2649240120
youngformoza <vlassova.yulya@gmail.com>
2021-07-14 23:02:47 +0300

    Добавлен репозиторий для работы с БД должностей сотрудников

---
+++ DB_service_Infrastructure/MySQLRepositories/MySQLPositionsRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DB_Connections.Entities;
+using DB_Connections.Interfaces;
+using MySql.Data.MySqlClient;
+
+namespace DB_service_Infrastructure.MySQLRepositories
+{
+    class MySQLPositionsRepository
+    {
+        protected string ConnectionString { get; set; }
+
+
+        public MySQLPositionsRepository()
+        {
+            ConnectionString = "server=localhost;user=root;database=service_center;port=3306;password=2907Yu1909Ya";
+        }
+        
+
+        public positions GetById(int id)
+        {
+            positions ChPosition = null;
+
+            try
+            {
+                using var connection = new MySqlConnection(ConnectionString);
+
+                connection.Open();
+
+                using var command = new MySqlCommand("SELECT id, name FROM positions WHERE id_pos = @id", connection);
+
+                command.Parameters.AddWithValue("id", id);
+
+                using var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ChPosition = new positions(reader.GetInt32(0), reader.GetString(1));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return ChPosition;
+        }
+                                
+    }
+}
