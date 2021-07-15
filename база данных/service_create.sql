87e13d158ab0fe1403202be7734559207aff45ce
youngformoza <vlassova.yulya@gmail.com>
2021-07-15 19:22:34 +0300

    Создание и заполнение базы данных

---
+++ база данных/service_create.sql
@@ -0,0 +1,130 @@
+CREATE SCHEMA service_center;
+
+USE service_center;
+
+CREATE TABLE customers (
+    id_cus INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `position` VARCHAR(50) NOT NULL,
+    birthday DATE NOT NULL,
+    mail VARCHAR(100) NOT NULL,
+    phone BIGINT UNSIGNED NOT NULL
+);
+
+CREATE TABLE services (
+    id_ser INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE equipment_class (
+    id_eq_cl INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE vendor (
+    id_ven INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE `position` (
+    id_pos INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE `status` (
+    id_stat INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE equipment (
+    id_eq INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    series VARCHAR(5) NOT NULL,
+    id_class INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_equipment_class_id FOREIGN KEY (id_class)
+		REFERENCES equipment_class (id_eq_cl),
+	id_vendor INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_equipment_vendor_id FOREIGN KEY (id_vendor)
+		REFERENCES vendor (id_ven)   
+);
+
+CREATE TABLE details (
+    id_det INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    part_number VARCHAR(100) NOT NULL,
+    note VARCHAR(100),
+    quantity INT UNSIGNED NOT NULL,  
+	id_vendor INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_details_vendor_id FOREIGN KEY (id_vendor)
+		REFERENCES vendor (id_ven)   
+);
+
+CREATE TABLE detail_to_equipment (
+    id_equipment INT UNSIGNED NOT NULL,
+    id_detail INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id_equipment , id_detail),
+    CONSTRAINT FOREIGN KEY (id_equipment)
+        REFERENCES equipment (id_eq),
+    CONSTRAINT FOREIGN KEY (id_detail)
+        REFERENCES details (id_det) 
+);
+
+CREATE TABLE employees (
+    id_empl INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    qualification VARCHAR(100) NOT NULL,
+	id_position INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_employees_position_id FOREIGN KEY (id_position)
+		REFERENCES `position` (id_pos)   
+);
+
+CREATE TABLE requests (
+    id_req INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	date_time_start DATETIME NOT NULL,
+    date_time_end DATETIME,
+    urgency VARCHAR(20) NOT NULL,
+    id_customer INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_requests_customers_id FOREIGN KEY (id_customer)
+		REFERENCES customers (id_cus),
+	id_equipment INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_requests_equipment_id FOREIGN KEY (id_equipment)
+		REFERENCES equipment (id_eq),
+	id_service INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_requests_services_id FOREIGN KEY (id_service)
+		REFERENCES services (id_ser),
+    id_employee_reception INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_requests_employees_rec_id FOREIGN KEY (id_employee_reception)
+		REFERENCES employees (id_empl),
+	id_employee_engineer INT UNSIGNED,
+    CONSTRAINT fk_requests_employees_eng_id FOREIGN KEY (id_employee_engineer)
+		REFERENCES employees (id_empl),
+	id_status INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_requests_status_id FOREIGN KEY (id_status)
+		REFERENCES `status` (id_stat)
+);
+
+CREATE TABLE invoices (
+    id_inv INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	date_time_start DATETIME NOT NULL,
+    date_time_end DATETIME,
+    note VARCHAR(100),
+    id_employee_engineer INT UNSIGNED,
+    CONSTRAINT fk_invoices_employees_eng_id FOREIGN KEY (id_employee_engineer)
+		REFERENCES employees (id_empl),
+	id_employee_provider INT UNSIGNED,
+    CONSTRAINT fk_invoices_employees_rec_id FOREIGN KEY (id_employee_provider)
+		REFERENCES employees (id_empl),
+	id_status INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_invoices_status_id FOREIGN KEY (id_status)
+		REFERENCES `status` (id_stat)
+);
+
+CREATE TABLE detail_to_invoice (
+    id_detail INT UNSIGNED NOT NULL,
+    id_invoice INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id_detail , id_invoice),
+    CONSTRAINT FOREIGN KEY (id_detail)
+        REFERENCES details (id_det),
+    CONSTRAINT FOREIGN KEY (id_invoice)
+        REFERENCES invoices (id_inv),
+	quantity INT UNSIGNED NOT NULL  
+);
---
+++ база данных/service_inserts.sql
@@ -0,0 +1,101 @@
+LOAD DATA INFILE  'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/customers_s.csv'
+INTO TABLE customers
+FIELDS TERMINATED BY ';' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+INSERT INTO vendor (`name`) VALUES
+('Xerox'),
+('HP'),
+('Konica Minolta'),
+('Rowe'),
+('Samsung');
+
+INSERT INTO equipment_class (`name`) VALUES
+('Versalink'),
+('Altalink'),
+('Versant'),
+('Ecoprint'),
+('ROWE Scan'),
+('SCX'),
+('SF');
+
+INSERT INTO equipment (series, id_class, id_vendor) VALUES
+('3025', 4, 5),
+('C8130', 2, 1),
+('B8145', 2, 1),
+('B205', 1, 1),
+('B615', 1, 1),
+('i4', 4, 4),
+('i6', 4, 4), 
+('i8', 4, 4),
+('i10', 4, 4), 
+('450i', 5, 4), 
+('850i', 5, 4),
+('8240', 6, 5),
+('345', 7, 5);
+
+INSERT INTO details (`name`, part_number, note, quantity, id_vendor) VALUES
+('Барабан', '013R00662', 'оригинальный', 10, 1),
+('Тонер', '006R01702', 'голубой', 8, 1),
+('Ролик переноса', '115R00116', 'оригинальный', 1, 1),
+('Картридж', 'C9455A', 'светло-пурпурный', 2, 2),
+('Картридж', 'C9454A', 'желтый', 2, 2),
+('Печка в сборе', 'A3PEPP3V01', 'bizhub 226', 1, 3),
+('Барабан', 'BT0000/07/00/001', null, 1, 4),
+('Нить заряда', 'MA0000/10/00/995', null, 5, 4),
+('Озоновый фильтр', 'BT0000/36/00/047', null, 10, 4),
+('Плата ADF', 'JC92-02446B', null, 1, 5);
+
+INSERT INTO detail_to_equipment (id_equipment, id_detail) VALUES
+(1, 10),
+(2, 2),
+(3, 1),
+(6, 8),
+(7, 7),
+(10, 9),
+(4, 3),
+(2, 1);
+
+INSERT INTO services (`name`) VALUES
+('Диагностика'),
+('Техническое обслуживание'),
+('Ремонт');
+
+INSERT INTO `status` (`name`) VALUES
+('Новое'),
+('Принято сотрудником'),
+('Ожидает деталь'),
+('Ожидает заказчика'),
+('Завершено');
+
+INSERT INTO `position` (`name`) VALUES
+('Инженер'),
+('Кладовщик'), 
+('Диспетчер');
+
+LOAD DATA INFILE  'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/employee_s.csv'
+INTO TABLE employees
+FIELDS TERMINATED BY ';' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+INSERT INTO requests (date_time_start, date_time_end, urgency, id_customer, id_equipment, id_service, id_employee_reception, id_employee_engineer, id_status) VALUES
+('20200225134040', '20200302152020', 'обычная', 2, 2, 1, 1, 6, 4),
+('20210628154000', null, 'обычная', 1, 4, 2, 8, 7, 3),
+('20200301130040', '20200303152020', 'высокая', 5, 3, 3, 1, 2, 4),
+('20210708154000', null, 'высокая', 7, 9, 1, 8, 10, 1);
+
+INSERT INTO invoices (date_time_start, date_time_end, note, id_employee_engineer, id_employee_provider,  id_status) VALUES
+('20200226204040', '20200228152020',null, 6, 3, 4),
+('20210630204000', null, null, 7, 4, 2),
+('20200301150040', '20200302082020', null, 2, 3, 4),
+('20210709150000', null, null, 8, 3, 1);
+
+INSERT INTO detail_to_invoice (id_detail, id_invoice, quantity) VALUES
+(1, 3, 1),
+(3, 2, 5),
+(4, 1, 2), 
+(2, 4, 3);
